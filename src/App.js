23bf0eb5b1fced9b6ead21bddea5463f9cc16162
pkji <pk@gmail.com>
2024-03-15 17:14:23 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+       <Header></Header>
+      <h1 className='text-[50px] text-red-900 font-bold'>priyanka sharma</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import  './Header.css'
+
+function Header() {
+  return (
+    <div>
+      <p className='header'>developer</p>
+      <p className='para'>hello program</p>
+      <p className='last'>developer priyanka</p>
+    </div>
+  )
+}
+
+export default Header
